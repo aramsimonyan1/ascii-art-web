0d221ce88987aa724dd18fa517ae648b230a67a9
Your Name <you@example.com>
2023-06-14 23:57:04 +0100

    new

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import {
+	"fmt"
+	"net/http"
+}
+
+func main() {
+
+	//create a file server handler for serving static files
+	fs := http.FileServer(http.Dir("./static"))
+
+	//register the file server hadler for the "/static/" route
+	http.Handle("/static/"), http.StripPrefix("/static/", fs)
+
+	fmt.Println("Server listening on port 8080...")
+	http.ListenAndServe("8080", nil)
+}
