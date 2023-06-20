f4e35e5791ab21b3c34ff7bbf68b6e9d3a315c18
Your Name <you@example.com>
2023-06-20 13:35:25 +0100

    notes

--- main.go
+++ main.go
@@ -9,28 +9,22 @@ import (
 )
 
 func main() {
-	// Create a file server handler for serving static files
 	fs := http.FileServer(http.Dir("templates"))
-	// Register the file server handler for the "/static/" route
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
 
-	// This line registers the homeHandler function as the handler for the root URL ("/") of the server. When a request is made to the root URL, the homeHandler function will be executed.
 	http.HandleFunc("/", homeHandler)
 
-	// This line registers the asciiArtHandler function as the handler for the "/ascii-art" URL of the server. When a request is made to the "/ascii-art" URL, the asciiArtHandler function will be executed.
+
 	http.HandleFunc("/ascii-art", asciiArtHandler)
 
-	/* This line starts the HTTP server and listens for incoming requests on port 8080. The the 1st argument is the address and
-	port to listen on (in this case, ":8080" means to listen on all available network interfaces on port 8080), and the 2nd argument is
-	the handler to use for incoming requests (in this case, nil means to use the default handler, which is DefaultServeMux). */
+
 	err := http.ListenAndServe(":8080", nil)
-	if err != nil { // Checks if there was an error starting the server. If an error occurs, such as if the port is already in use, it will cause the program to panic, meaning it will immediately stop execution and print the error message.
+	if err != nil { 
 		panic(err)
 	}
 }
 
-// The homeHandler func handles the GET request for the main page, where the user can input their text and select the banner.
-// if the template files (index.html or result.html) are not found, the handlers will return a "Template not found" error response
+
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "GET" {
 		tmpl, err := template.ParseFiles("templates/index.html")
@@ -42,7 +36,7 @@ func homeHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
-// The asciiArtHandler func handles the POST request when the user submits the form, generates the ASCII art, and renders the result in the result.html template.
+
 func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 	if r.Method == "POST" {
 		err := r.ParseForm()
@@ -54,7 +48,6 @@ func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 		inputText := r.Form.Get("inputText")
 		banner := r.Form.Get("banners")
 
-		// validation check for the input before generating the ASCII art (clicking the button 'ASCII art' without any input)
 		if inputText == "" {
 			http.Error(w, "Input text is required", http.StatusBadRequest)
 			return
@@ -62,28 +55,24 @@ func asciiArtHandler(w http.ResponseWriter, r *http.Request) {
 
 		art, err := generateASCIIArt(inputText, banner)
 		if err != nil {
-			// when the ASCII art generation encounters an unhandled errors
 			http.Error(w, "Failed to generate ASCII art", http.StatusInternalServerError)
 			return
 		}
 
-		// tmpl := template.Must(template.ParseFiles("templates/result.html"))
 		tmpl, err := template.ParseFiles("templates/result.html")
 		if err != nil {
 			http.Error(w, "Template not found", http.StatusNotFound)
 			return
 		}
 		tmpl.Execute(w, art)
-		// If a request method other than POST is received, it returns a "Method Not Allowed" error with the appropriate status code (http.StatusMethodNotAllowed).
 	} else {
 		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 	}
 }
 
-// The generateASCIIArt handles the conversion of user input into ASCII art using the selected banner.
 func generateASCIIArt(text, banner string) (string, error) {
 	var filename string
-	switch banner { // switch statement that checks the value/name of the banner variable
+	switch banner { 
 	case "shadow":
 		filename = "shadow.txt"
 	case "standard":
@@ -94,44 +83,20 @@ func generateASCIIArt(text, banner string) (string, error) {
 		return "", nil
 	}
 
-	// Attempts to open the file specified by the filename variable. If successful, it returns a file descriptor (file) and a nil error.
 	file, err := os.Open(filename)
 	if err != nil {
 		return "", err
 	}
-	defer file.Close() // schedules the Close() function of the file to be called when the generateASCIIArt completes.
+	defer file.Close() 
 
-	/* This code segment reads the content of a specific file (based on the banner value)
-	line by line and stores each line in the fileLines slice. It sets up the necessary
-	file handling and prepares the content for later use in generating the ASCII art.*/
 
-	fileScanner := bufio.NewScanner(file) // creates a scanner (fileScanner) to read the contents of the opened file.
-	fileScanner.Split(bufio.ScanLines)    // configures the scanner to split the file content into lines based on newline characters ('\n').
-	var fileLines []string                // declares an empty slice (fileLines) to store the lines read from the file.
-	for fileScanner.Scan() {              // enters a loop that iterates until the scanner reaches the end of the file.
-		fileLines = append(fileLines, fileScanner.Text()) // in each iteration, it reads the next line from the file using fileScanner.Scan() and adds the line to the fileLines slice using fileScanner.Text()
+	fileScanner := bufio.NewScanner(file) 
+	fileScanner.Split(bufio.ScanLines)    
+	var fileLines []string                
+	for fileScanner.Scan() {              
+		fileLines = append(fileLines, fileScanner.Text()) 
 	}
 
-	/*  This piece of code wil do multiple lines if input text contains '\n'
-	// looking for "\n" and turn it into "n3wL1ne" so string.Split can find it
-	preLine := []rune(text)
-	for m := 0; m < len(preLine); m++ {
-		arrayMiddle := "n3wL!Ne"
-		if preLine[m] == 92 && preLine[m+1] == 'n' {
-			array1 := preLine[0:m]
-			array2 := preLine[m+2:]
-			s1 := string([]rune(array1))
-			s2 := string([]rune(array2))
-			text = s1 + arrayMiddle + s2
-			preLine = []rune(text)
-		}
-	}
-
-	// split the text into lines if required
-	lines := strings.Split(string(preLine), "n3wL!Ne")
-	*/
-
-	// This line of code will do multiple lines when 'Enter' button is used on keybord for separate lines in input text
 	lines := strings.Split(text, "\n")
 
 	var generatedArt strings.Builder
@@ -146,9 +111,7 @@ func generateASCIIArt(text, banner string) (string, error) {
 		for k := 1; k < 9; k++ {
 			for _, ch := range runes {
 				m := rune(k)
-				// reduce each character value by 32 in ascii table,
-				// multiply by the 9 rows each character uses in standard.txt,
-				// add the row number
+
 				asciiFetch := ((ch - 32) * 9) + m
 				if int(asciiFetch) >= 0 && int(asciiFetch) < len(fileLines) {
 					generatedArt.WriteString(fileLines[int(asciiFetch)])
--- main_test.go
+++ main_test.go
@@ -8,103 +8,65 @@ import (
 	"testing"
 )
 
-/*
-Function checks whether your project handles the HTTP status code 404 - Not Found correctly.
-It simulates a request to a non-existent path (endpoint) and check if the application returns a "404 Not Found" status code.
-*/
+
 func TestNotFoundHandler(t *testing.T) {
-	// We create a GET request to the /non-existing-endpoint path, which is a non-existing endpoint in your application.
 	req, err := http.NewRequest("GET", "/non-existent", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Create a ResponseRecorder to record the response
 	rr := httptest.NewRecorder()
 
-	/* A method call that directly invokes the ServeHTTP method of the default HTTP request multiplexer (http.DefaultServeMux).
-	This approach bypasses the registered handler functions and directly uses the default multiplexer to handle the request.
-	It can be useful in certain scenarios, such as when you want to simulate requests to specific paths that are not handled
-	by your registered handlers.
-	Since there is no registered handler for the requested path, the default multiplexer will return a "404 Not Found" response.
-	*/
 	http.DefaultServeMux.ServeHTTP(rr, req)
 
-	// Check the response status code
 	if rr.Code != http.StatusNotFound {
 		t.Errorf("Expected status 404, but got %d", rr.Code)
 	}
 }
 
 func TestHomeHandler(t *testing.T) {
-	// Create a request to pass to the handler
 	req, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	// Create a ResponseRecorder to record the response
 	rr := httptest.NewRecorder()
 
-	// Call the homeHandler function directly, passing in the ResponseRecorder and Request
 	homeHandler(rr, req)
 
-	// Check the response status code
 	if rr.Code != http.StatusOK {
 		t.Errorf("Expected status 200, but got %d", rr.Code)
 	}
 }
 
-/* We create the request using http.NewRequest("POST", "/ascii-art", 1), but we set
-the "Content-Type" header to "application/x-www-form-urlencoded" explicitly.
-This indicates that the request will contain form data. */
 func TestBadRequestHandler(t *testing.T) {
-	// Create a request to pass to the handler
 	req, err := http.NewRequest("POST", "/ascii-art", nil)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	// Set the Content-Type header to indicate form data
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	// Create a ResponseRecorder to record the response
 	rr := httptest.NewRecorder()
 
-	// Call the asciiArtHandler function directly, passing in the ResponseRecorder and Request
 	asciiArtHandler(rr, req)
 
-	// Check the response status code
 	if rr.Code != http.StatusBadRequest {
 		t.Errorf("Expected status 400, but got %d", rr.Code)
 	}
 }
 
-/*
-Function is responsible for testing the handling of an internal server error (HTTP status code 500) by the server.
-*/
-func TestInternalServerErrorHandler(t *testing.T) {
-	// We create a ResponseRecorder (rr) to capture the response.
+
+func TestInternalServerErrorHandler(t *testing.T) {.
 	rr := httptest.NewRecorder()
 
-	// Then, we simulate an internal server error by calling http.Error and passing the rr recorder,
-	// the error message ("Internal Server Error"), and the status code http.StatusInternalServerError.
 	http.Error(rr, "Internal Server Error", http.StatusInternalServerError)
 
-	// We check the response status code (rr.Code) and compare it with the expected value (http.StatusInternalServerError).
-	// If the status code doesn't match the expected value, we report an error indicating the mismatch.
 	if rr.Code != http.StatusInternalServerError {
 		t.Errorf("Expected status 500, but got %d", rr.Code)
 	}
 }
 
-/* Does the test send a valid request to the asciiArtHandler function?
-We create a formData variable that holds the required form data with "inputText" and "banners" fields.
-We then encode the form data and set it as the request body using strings.NewReader(formData.Encode()).
-Additionally, we set the "Content-Type" header to "application/x-www-form-urlencoded" to indicate the form data format.
-*/
 func TestAsciiArtHandler(t *testing.T) {
-	// Create a request to pass to the handler
 	formData := url.Values{
 		"inputText": []string{"example text"},
 		"banners":   []string{"shadow"},
@@ -115,31 +77,24 @@ func TestAsciiArtHandler(t *testing.T) {
 	}
 	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
 
-	// Create a ResponseRecorder to record the response
 	rr := httptest.NewRecorder()
 
-	// Call the asciiArtHandler function directly, passing in the ResponseRecorder and Request
 	asciiArtHandler(rr, req)
 
-	// Check the response status code
 	if rr.Code != http.StatusOK {
 		t.Errorf("Expected status 200, but got %d", rr.Code)
 	}
 }
 
 func TestGenerateASCIIArt(t *testing.T) {
-	// Create test inputs
 	text := "Hello"
 	banner := "standard"
 
-	// Call the generateASCIIArt function with the test inputs
 	result, err := generateASCIIArt(text, banner)
-	// Check for errors
 	if err != nil {
 		t.Errorf("Failed to generate ASCII art: %v", err)
 	}
 
-	// Check if the result is not empty
 	if result == "" {
 		t.Errorf("Expected non-empty result, but got an empty string")
 	}
